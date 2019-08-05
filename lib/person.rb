2df989a3f91fe2ef9dafd06d115c8bbf587f8bef
Patrick Harrison <jahman@bellatlantic.net>
2019-08-05 22:31:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
 class Person
   def talk
-    puts "Hello World!!"
+    puts "Hello World!"
   end
    def walk
     
-   puts "The Peson is walking"
+   puts "The Person is walking"
    
   end
 end
